575529451be6e6eaf482615e7747fd2d534cc960
tararamgoyal <tararamgoyal4@gmail.com>
2023-12-19 10:44:13 +0530

    ehancement in link componts

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,13 +2,18 @@ import { useContext } from "react";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
+
     const { navigate } = useContext(NavigationContext);
+
     const handleClick = (event) => {
-        event.preventDefault();
+        if (event.metaKey || event.ctrlKey){
+            return;
+        }
+            event.preventDefault();
         navigate(to);
     };
-    
-    return <a onClick={handleClick} >{children}</a>
+
+    return <a href={to} onClick={handleClick} >{children}</a>
 
 }
 
